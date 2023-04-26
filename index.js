7c8d0ad30f5daccdd195128002240457a61dc1c8
msoler75 <msoler75@yahoo.es>
2023-04-26 11:13:08 -0400

    locale working

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 import exec from "./lib/exec.js";
 import fs from "fs";
 import SintesisError from "./src/SintesisError.js";
-import {i18t} from './locale.js'
-const command_arg = process.argv[2];
-
-if (!command_arg) {
-  console.log("Debes especificar un archivo o un programa");
-  process.exit();
-}
+import {loadLocales, translate, _t} from './lib/locale.js'
 
 const ucfirst = (text) => {
   return text.charAt(0).toUpperCase() + text.slice(1)
 }
 
 
-let sintesis_code = command_arg;
-let file = command_arg;
-if (file.match(/.*\.s(in|intesis)?$/) || fs.existsSync(file)) {
-  try {
-    sintesis_code = fs.readFileSync(file, "utf8");
-  } catch (err) {
-    console.log(`Error al leer el archivo ${file}`);
+
+
+loadLocales().then(()=>{
+
+  const command_arg = process.argv[2];
+
+  if (!command_arg) {
+    console.log(ucfirst(_t("args-expected")));
     process.exit();
   }
-}
 
-if (sintesis_code) {
-  const lines = sintesis_code.split("\n");
-
-  async function mostrarError(titulo, err) {
-    let line = lines[err.line - 1];
-    let numline = "Línea " + err.line + ":   ";
-    console.log(numline + line);
-    console.log(
-      " ".repeat(numline.length + err.column) +
-        "^".repeat(err.stop - err.start + 1)
-    );
-    console.log(
-      ucfirst(titulo),
-      err.msg
-      .replace("no viable alternative at input", await i18t("no-se-esperaba"))
-        .replace("extraneous", await i18t("extrana"))
-        .replace("input", await i18t("entrada"))
-        .replace("expecting", await i18t("se-esperaba"))
-        .replace("mismatched", await i18t("no-coincide"))
-        .replace("alternative", await i18t("alternativa"))
-        .replace(/\bat\b/, await i18t("en"))
-        .replace("missing", await i18t("se-esperaba"))
-    );
+  let sintesis_code = command_arg;
+  let file = command_arg;
+  if (file.match(/.*\.s(in|intesis)?$/) || fs.existsSync(file)) {
+    try {
+      sintesis_code = fs.readFileSync(file, "utf8");
+    } catch (err) {
+      console.log(`Error al leer el archivo ${file}`);
+      process.exit();
+    }
   }
 
-  try {
-    console.log(exec(sintesis_code));
-  } catch (err) {
-    if (err instanceof SyntaxError) {
-      const data = JSON.parse(err.message);
-      await mostrarError(await i18t("error-sintaxis") + ":", data);
-    } else if (err instanceof SintesisError) {
-      //SintesisError
-      await mostrarError(await i18t("error-ejecucion") + ":", err);
-    } else console.error(err);
+  if (sintesis_code) {
+    const lines = sintesis_code.split("\n");
+
+    function mostrarError(titulo, err) {
+      let line = lines[err.line - 1];
+      let numline = "Línea " + err.line + ":   ";
+      console.log(numline + line);
+      console.log(
+        " ".repeat(numline.length + err.column) +
+          "^".repeat(err.stop - err.start + 1)
+      );
+      console.log(
+        ucfirst(titulo),
+        err.msg
+        .replace("no viable alternative at input", _t("no-se-esperaba"))
+          .replace("extraneous", _t("extrana"))
+          .replace("input", _t("entrada"))
+          .replace("expecting", _t("se-esperaba"))
+          .replace("mismatched", _t("no-coincide"))
+          .replace("alternative", _t("alternativa"))
+          .replace(/\bat\b/, _t("en"))
+          .replace("missing", _t("se-esperaba"))
+      );
+    }
+
+    try {
+      console.log(exec(sintesis_code, translate));
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        const data = JSON.parse(err.message);
+        mostrarError(_t("error-sintaxis") + ":", data);
+      } else if (err instanceof SintesisError) {
+        //SintesisError
+        mostrarError(_t("error-ejecucion") + ":", err);
+      } else console.error(err);
+    }
   }
-}
+
+})
